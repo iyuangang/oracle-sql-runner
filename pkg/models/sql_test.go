8f3bf924e3ed8d36b1581832f8cd9760463fbeb6
Tim Yuan <yuangang@me.com>
2024-11-27 15:59:28 +0800

    fix: ci run fail

--- pkg/models/sql_test.go
+++ pkg/models/sql_test.go
@@ -158,6 +158,7 @@ func TestResult_Print(t *testing.T) {
 				_, err := io.Copy(&buf, r)
 				if err != nil {
 					t.Errorf("io.Copy error: %v", err)
+
 				}
 				outputChan <- buf.String()
 			}()
